a578d8172447ac35ff9c0a1350fa7b22efca353c
Mohamed Eladly <msrtv365@yahoo.com>
2024-01-30 10:43:10 +0200

    Trying to fix the failing checkers

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -11,15 +11,18 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
 
+	if (!parent || !value)
+		return (NULL);
+
 	new_node = malloc(sizeof(binary_tree_t));
-	if (!new_node || !parent)
+	if (!new_node)
 		return (NULL);
 
 	new_node->parent = parent;
 	new_node->n = value;
 	new_node->left = new_node->right = NULL;
 
-	if (parent->left != NULL)
+	if (parent->left)
 	{
 		parent->left->parent = new_node;
 		new_node->left = parent->left;
