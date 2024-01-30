3cdb24bd4e59980955f1448558d8ce51302fe126
Radouane Abounouas <redouane1997.ocs@gmail.com>
2024-01-30 17:32:35 +0100

    task 19

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,29 +1,79 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_depth - measures the depth of a node in a binary tree
+ *
+ * @tree: pointer to the node to measure the depth
+ * Return: the depth, 0 otherwise.
+ **/
+
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	if (!tree || !tree->parent)
+		return (0);
+
+	return (binary_tree_depth(tree->parent) + 1);
+}
+
+/**
+ * get_ancestor - This is helper function that returns the lowest common
+ *	ancestor of two nodes at the same depth
+ *
+ * @first:  a pointer to the first node
+ * @second: a pointer to the second node
+ *
+ * Return: pointer to the ancestor node
+ */
+
+binary_tree_t *get_ancestor(const binary_tree_t *first,
+		const binary_tree_t *second)
+{
+	binary_tree_t *common_ancestor = first->parent;
+
+	if (first == second)
+		return ((binary_tree_t *)first);
+
+	if (first->parent != second->parent)
+		common_ancestor = get_ancestor(first->parent, second->parent);
+
+	return (common_ancestor);
+}
+
+/**
  * binary_trees_ancestor - finds the lowest common ancestor of two nodes
  *
  * @first:  a pointer to the first node
  * @second: a pointer to the second node
  * Return: pointer to the ancestor node
  **/
+
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
-									 const binary_tree_t *second)
+		const binary_tree_t *second)
 {
-	binary_tree_t *f_an, *s_an;
+	size_t f_node, s_node, i;
+	const binary_tree_t *tmp;
 
 	if (!first || !second)
 		return (NULL);
 
-	f_an = first->parent;
-	s_an = second->parent;
+	f_node = binary_tree_depth(first);
+	s_node = binary_tree_depth(second);
 
-	if (s_an == f_an)
-		return (f_an);
-	else if (first == s_an)
-		return (s_an);
-	else if (second == f_an)
-		return (f_an);
+	if (f_node == s_node)
+		return (get_ancestor(first, second));
 
-	return (binary_trees_ancestor(first->parent, second->parent));
+	if (f_node > s_node)
+	{
+		tmp = first;
+		for (i = f_node; i > s_node; i--)
+			tmp = tmp->parent;
+		return (get_ancestor(tmp, second));
+	}
+	else
+	{
+		tmp = second;
+		for (i = s_node; i > f_node; i--)
+			tmp = tmp->parent;
+		return (get_ancestor(first, tmp));
+	}
 }
