8fce4c0bb477352c2cd63acc842db0992a1556fc
Radouane Abounouas <redouane1997.ocs@gmail.com>
2024-01-30 16:55:52 +0100

    my tasks

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,36 @@
+#include "binary_trees.h"
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+    size_t height = 0, h_l = 0, h_r = 0;
+
+    if (!tree || (!tree->left && !tree->right))
+        return (0);
+    
+    h_l = binary_tree_height(tree->left);
+    h_r = binary_tree_height(tree->right);
+
+    if (h_l > h_r)
+        height += h_l + 1;
+    else
+        height += h_r + 1;
+
+    return (height);
+}
+
+int binary_tree_balance(const binary_tree_t *tree)
+{
+    int h_l = -1, h_r = -1, balance = 0;
+
+    if (!tree || (!tree->left && !tree->right))
+        return (0);
+
+    if (tree->left)
+        h_l = binary_tree_height(tree->left);
+    if (tree->right)
+        h_r = binary_tree_height(tree->right);
+
+    balance = h_l - h_r;
+
+    return (balance);
+}
