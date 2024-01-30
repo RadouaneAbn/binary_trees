30e1db3b47bd6e371f29d367d8ed93bb85850dbb
Radouane Abounouas <redouane1997.ocs@gmail.com>
2024-01-30 20:32:49 +0100

    Task 14 correct method

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -9,20 +9,20 @@
  **/
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    size_t height = 0, h_l = 0, h_r = 0;
+	size_t height = 0, h_l = 0, h_r = 0;
 
-    if (!tree || (!tree->left && !tree->right))
-        return (0);
-    
-    h_l = binary_tree_height(tree->left);
-    h_r = binary_tree_height(tree->right);
+	if (!tree || (!tree->left && !tree->right))
+		return (0);
 
-    if (h_l > h_r)
-        height += h_l + 1;
-    else
-        height += h_r + 1;
+	h_l = binary_tree_height(tree->left);
+	h_r = binary_tree_height(tree->right);
 
-    return (height);
+	if (h_l > h_r)
+		height += h_l + 1;
+	else
+		height += h_r + 1;
+
+	return (height);
 }
 
 /**
@@ -34,17 +34,17 @@ size_t binary_tree_height(const binary_tree_t *tree)
  **/
 int binary_tree_balance(const binary_tree_t *tree)
 {
-    int h_l = -1, h_r = -1, balance = 0;
+	int h_l = -1, h_r = -1, balance = 0;
 
-    if (!tree || (!tree->left && !tree->right))
-        return (0);
+	if (!tree || (!tree->left && !tree->right))
+		return (0);
 
-    if (tree->left)
-        h_l = binary_tree_height(tree->left);
-    if (tree->right)
-        h_r = binary_tree_height(tree->right);
+	if (tree->left)
+		h_l = binary_tree_height(tree->left);
+	if (tree->right)
+		h_r = binary_tree_height(tree->right);
 
-    balance = h_l - h_r;
+	balance = h_l - h_r;
 
-    return (balance);
+	return (balance);
 }
