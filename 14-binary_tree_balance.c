cd25859fcfc3521ffd0e9424eb6eee7280749e84
Radouane Abounouas <redouane1997.ocs@gmail.com>
2024-01-30 20:35:44 +0100

    Task 14 correct method update

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -7,6 +7,7 @@
  * to measure the height.
  * Return: the height of the @tree, 0 otherwise.
  **/
+
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t height = 0, h_l = 0, h_r = 0;
@@ -32,9 +33,10 @@ size_t binary_tree_height(const binary_tree_t *tree)
  * to measure the height.
  * Return: the balance factor of the @tree.
  **/
+
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int h_l = -1, h_r = -1, balance = 0;
+	int h_l = -1, h_r = -1;
 
 	if (!tree || (!tree->left && !tree->right))
 		return (0);
@@ -44,7 +46,5 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (tree->right)
 		h_r = binary_tree_height(tree->right);
 
-	balance = h_l - h_r;
-
-	return (balance);
+	return (h_l - h_r);
 }
