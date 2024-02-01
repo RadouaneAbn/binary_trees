9af4535af412fdbb8658f4e10f6f8bcb811ea3c6
Radouane Abounouas <redouane1997.ocs@gmail.com>
2024-02-01 03:37:05 +0100

    Task 28 - hundle node is leaf

--- 114-bst_remove.c
+++ 114-bst_remove.c
@@ -126,10 +126,11 @@ bst_t *bst_remove(bst_t *root, int value)
 			parent->right = NULL;
 		free(node);
 	}
+    else
+	    remove(parent, node, succ);
 	if (!parent)
 		root = succ;
 
-	remove(parent, node, succ);
 
 	return (root);
 }
