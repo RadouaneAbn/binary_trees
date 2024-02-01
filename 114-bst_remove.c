60f165aa0012e42b4f7f6efc6baa2485b11153b7
Radouane Abounouas <redouane1997.ocs@gmail.com>
2024-02-01 03:11:19 +0100

    Task 28 - C: 4, 5, 7

--- 114-bst_remove.c
+++ 114-bst_remove.c
@@ -1,6 +1,32 @@
 #include "binary_trees.h"
 
 /**
+ * bst_search - This function searches for a value in Binary Search Tree
+ * @tree: Pointer to the BST
+ * @value: the value to be searched for
+ *
+ * Return: Pointer to the node with the wanted value if found
+ *  or NULL if tree is NULL or if nothing found
+ */
+
+bst_t *bst_search(const bst_t *tree, int value)
+{
+	if (!tree)
+		return (NULL);
+
+	if (value == tree->n)
+		return ((bst_t *)tree);
+
+	if (tree->left && value < tree->n)
+		return (bst_search(tree->left, value));
+
+	if (tree->right && value > tree->n)
+		return (bst_search(tree->right, value));
+
+	return (NULL);
+}
+
+/**
  * min_value - This functino get teh min value of a tree
  * @root: Pointer to the root of a tree
  *
@@ -98,30 +124,4 @@ bst_t *bst_remove(bst_t *root, int value)
 		root = succ;
 
 	return (root);
-}
-
-/**
- * bst_search - This function searches for a value in Binary Search Tree
- * @tree: Pointer to the BST
- * @value: the value to be searched for
- *
- * Return: Pointer to the node with the wanted value if found
- *  or NULL if tree is NULL or if nothing found
- */
-
-bst_t *bst_search(const bst_t *tree, int value)
-{
-	if (!tree)
-		return (NULL);
-
-	if (value == tree->n)
-		return ((bst_t *)tree);
-
-	if (tree->left && value < tree->n)
-		return (bst_search(tree->left, value));
-
-	if (tree->right && value > tree->n)
-		return (bst_search(tree->right, value));
-
-	return (NULL);
 }
